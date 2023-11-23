fd98629d00ce7f5e7a9f6ae81d4482e687e0eb69
Sergio <sercelaya@gmail.com>
2023-11-23 20:41:04 +0100

    Añadido el routing

--- src/routes/api/pedidos.js
+++ src/routes/api/pedidos.js
@@ -1,7 +1,21 @@
 const router = require("express").Router();
-const AutoresController = require("../../controllers/pedidos.controller");
+const PedidosController = require("../../controllers/pedidos.controller");
+
 
 //GET
-router.get("/", );
+router.get("/",PedidosController.getAllPedidos );
+router.get("/:idPedido",PedidosController.getPedidoById);
+router.get("/byEmpleadoId/:usuario_asignado",PedidosController.getAllPedidosByIdEmpleado);
+router.get("/byEmpleadoEstado/:usuario_asignado/:estado",PedidosController.getPedidosByIdEmpleadoEstado);
+//CREATE
+router.post("/",PedidosController.createPedido);
+//UPDATE
+router.put("/:idPedido",PedidosController.updatePedido);
+router.put("/toPendienteValidar/:idPedido",PedidosController.toPendienteValidar);
+router.put("/toRectificar/:idPedido",PedidosController.toRectificar);
+router.put("/toValidado/:idPedido",PedidosController.toValidado);
+router.put("/toEnTransito/:idPedido",PedidosController.toEnTransito);
+router.put("/toPendienteRecepcionar/:idPedido",PedidosController.toPendienteRecepcionar);
+router.put("/toFinalizado/:idPedido",PedidosController.toFinalizado);
 
 module.exports = router;
