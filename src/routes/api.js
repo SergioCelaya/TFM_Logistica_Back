e71fc405bc5df86794e25c1d994149f3e1a1815d
Sergio <sercelaya@gmail.com>
2023-11-25 11:21:40 +0100

    Prueba conflicto

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 
 router.use("/pedidos", require("./api/pedidos"));
 router.use("/incidencias", require("./api/incidencias"));
-
+console.log("Hola");
 
 module.exports = router;
