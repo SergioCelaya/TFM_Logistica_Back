ed1fa215d197cbe4bc2aa9fd2508c2c986d9b1c5
Sonia-GE <sonia.fmayoral@gmail.com>
2023-11-30 20:23:48 +0100

    Corrección datos empleados

--- diseño_BBDD/scripts_datos/empleados.sql
+++ diseño_BBDD/scripts_datos/empleados.sql
@@ -1,6 +1,6 @@
 
 INSERT INTO `empleados` 
 (email,pwd,nombre,apellidos,puesto,idalmacen,num_empleado,activo,fecha_contratacion,imagen_empleado)
-VALUES (('luciano.garcia@example.com','$2a$08$J1ULCG1LZPQ/NcMywwnXG.GUTu2qFlWhPfVbX6F6ofE20mTqGx4eu','luciano','Garcia',1,1,'#00687',1,'2020-10-10 16:20:00',NULL),('Fermin.Lopez@example.com','$2a$08$hGOXVZmyc3m0cJBLB416K.AZb4hkDqiihGrcPOhSgWHpYnnjnujAS','Fermin','Lopez',2,1,'#00689',1,'2020-10-10 16:20:00',NULL),('Leire.rins@example.com','$2a$08$YvWru4ayzm8W/BJ6RH4T7O0Waym3wtjCkphRCz4x5cEk/.x41dOaa','Leire','Rins',3,1,'#00690',1,'2020-10-10 16:20:00',NULL);
+VALUES ('luciano.garcia@example.com','$2a$08$J1ULCG1LZPQ/NcMywwnXG.GUTu2qFlWhPfVbX6F6ofE20mTqGx4eu','luciano','Garcia',1,1,'#00687',1,'2020-10-10 16:20:00',NULL),('Fermin.Lopez@example.com','$2a$08$hGOXVZmyc3m0cJBLB416K.AZb4hkDqiihGrcPOhSgWHpYnnjnujAS','Fermin','Lopez',2,1,'#00689',1,'2020-10-10 16:20:00',NULL),('Leire.rins@example.com','$2a$08$YvWru4ayzm8W/BJ6RH4T7O0Waym3wtjCkphRCz4x5cEk/.x41dOaa','Leire','Rins',3,1,'#00690',1,'2020-10-10 16:20:00',NULL);
 
 
