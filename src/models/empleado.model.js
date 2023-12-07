ba65483fb413004d8a04a647ff11bc0d58b6ea96
albegosu <albegosu@gmail.com>
2023-12-07 03:24:20 +0100

    Agrego imagen_empleado a la llamada a getEmpleadoById para la foto del sidebar

--- src/models/empleado.model.js
+++ src/models/empleado.model.js
@@ -14,7 +14,7 @@ const getNumAllEmpleados = () => {
 
 const getEmpleadoById = (idEmpleado) => {
   return db.query(
-    "SELECT emp.idempleado,emp.email,emp.nombre,emp.apellidos,pst.descripcion puesto,emp.idalmacen,emp.num_empleado,emp.activo,emp.fecha_contratacion FROM empleados emp inner join puestos_trabajo pst on emp.puesto = pst.idpuesto_trabajo where emp.idempleado = ?",
+    "SELECT emp.idempleado,emp.email,emp.nombre,emp.apellidos,pst.descripcion puesto,emp.idalmacen,emp.num_empleado,emp.activo,emp.fecha_contratacion,emp.imagen_empleado FROM empleados emp inner join puestos_trabajo pst on emp.puesto = pst.idpuesto_trabajo where emp.idempleado = ?",
     [idEmpleado]
   );
 };
